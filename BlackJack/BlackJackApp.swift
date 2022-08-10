535f2480b258a7f2134bf032af45521601461e21
Dorian Clay <dorian@dorianclay.com>
2022-08-09 17:39:27 -0700

    Initial Commit

---
+++ BlackJack/BlackJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackApp.swift
+//  BlackJack
+//
+//  Created by Dorian Clay on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
